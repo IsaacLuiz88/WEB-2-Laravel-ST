985c301b637c3ea79254cd4e06c8743579775877
Isaac <isaacluiz0808@gmail.com>
2025-05-09 10:42:37 -0300

    Creating Books's table

---
+++ Atividade-02/database/migrations/2025_05_09_133743_create_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->integer('id')->primary()->autoIncrement();
+            $table->string('title', 255);
+            $table->unsignedBigInteger('author_id')->cascadeOnDelete();
+            $table->unsignedBigInteger('category_id')->cascadeOnDelete();
+            $table->unsignedBigInteger('publisher_id')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
