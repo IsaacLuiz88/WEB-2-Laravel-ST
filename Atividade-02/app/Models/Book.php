46a78023d38da5e33d8b5397bffe0d2b2bd7f707
Isaac <isaacluiz0808@gmail.com>
2025-05-09 11:18:34 -0300

    Improving models: adding protection and functions to models

--- Atividade-02/app/Models/Book.php
+++ Atividade-02/app/Models/Book.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['title', 'author_id', 'category_id', 'publisher_id', 'publication_year'];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function publisher()
+    {
+        return $this->belongsTo(Publisher::class);
+    }
 }
