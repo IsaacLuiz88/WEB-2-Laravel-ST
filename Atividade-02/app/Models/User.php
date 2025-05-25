dad2b12178e57c84b2139416bf749f73d4ff04e2
Isaac <isaacluiz0808@gmail.com>
2025-05-25 14:22:06 -0300

    Creating Borrowing's model; Adapting User's model to have N to N relationship

--- Atividade-02/app/Models/User.php
+++ Atividade-02/app/Models/User.php
@@ -23,6 +23,12 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function books(){
+        return $this->belongsToMany(Book::class, 'borrowings')
+            ->withPivot('borrowed_at', 'returned_at')
+            ->withTimestamps();
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
