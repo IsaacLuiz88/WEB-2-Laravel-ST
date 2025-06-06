9e2b4f0c63d91571b127c1ad000c3e18baff11d6
Isaac <isaacluiz0808@gmail.com>
2025-06-06 10:27:34 -0300

    Creating Book controller; adding routes of book controller in web.php

--- Atividade-02/routes/web.php
+++ Atividade-02/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PublisherController;
 use App\Http\Controllers\AuthorController;
+use App\Models\Book;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('categories', CategoryController::class);
 Route::resource('publishers', PublisherController::class);
 Route::resource('authors', AuthorController::class);
+
+Route::get('/books/create-id-number', [BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [BookController::class, 'storeWithId'])->name('books.store.id');
+
+Route::get('/books/create-select', [BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [BookController::class, 'storeWithSelect'])->name('books.store.select');
