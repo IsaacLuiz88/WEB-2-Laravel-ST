8144867cc6238e18575dcdff5511c5de0d5228c0
Isaac <isaacluiz0808@gmail.com>
2025-06-24 17:15:43 -0300

    Creting Policies for Autorization

---
+++ Atividade-02/app/Policies/PublisherPolicy.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Publisher;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PublisherPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        // Customers, Librarians, and Admins can view the list.
+        return $user->isClient() || $user->isAdminOrLibrarian();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Publisher $publisher): bool
+    {
+        // Customers, Librarians, and Admins can view a specific item.
+        return $user->isClient() || $user->isAdminOrLibrarian();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isAdminOrLibrarian();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Publisher $publisher): bool
+    {
+        return $user->isAdminOrLibrarian();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Publisher $publisher): bool
+    {
+        return $user->isAdminOrLibrarian();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Publisher $publisher): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Publisher $publisher): bool
+    {
+        return false;
+    }
+}
---
+++ Atividade-02/app/Policies/UserPolicy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+    public function before(User $user, string $ability): bool|null{
+        if($user->isAdmin()) {
+            return true; // Admin users can perform any action
+        }
+        return null;
+    }
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        // Customers, Librarians, and Admins can view the list.
+        return $user->isClient() || $user->isAdminOrLibrarian();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, User $model): bool
+    {
+        // Customers, Librarians, and Admins can view a specific item.
+        return $user->isClient() || $user->isAdminOrLibrarian();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isAdminOrLibrarian();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, User $model): bool
+    {
+        return $user->isAdminOrLibrarian();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $model): bool
+    {
+        return $user->isAdminOrLibrarian();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, User $model): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, User $model): bool
+    {
+        return false;
+    }
+}
