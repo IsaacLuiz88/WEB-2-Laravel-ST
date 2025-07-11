7cd166112beb3355ac58ca2ca52c48fc4f713360
Isaac <isaacluiz0808@gmail.com>
2025-07-11 10:43:51 -0300

    Adding functions to handle with borroweds late. Adding constants of day limit and payment;

--- Atividade-02/app/Models/Borrowing.php
+++ Atividade-02/app/Models/Borrowing.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Borrowing extends Model
 {
-   use HasFactory;
+    use HasFactory;
 
     protected $fillable = ['user_id', 'book_id', 'borrowed_at', 'returned_at',];
 
@@ -16,11 +17,37 @@ class Borrowing extends Model
         'returned_at' => 'datetime',
     ];
 
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function book() {
+    public function book()
+    {
         return $this->belongsTo(Book::class);
+    }
+
+    const returnLimitDays = 15;
+    const finePerDay = 0.50;
+
+    public function daysLate(): int
+    {
+        if ($this->returned_at == null) {
+            return 0;
+        }
+
+        $dueDate = $this->borrowed_at->copy()->addDays(self::returnLimitDays);
+
+        if ($this->returned_at->lessThanOrEqualTo($dueDate)) {
+            return 0;
+        }
+
+        return $this->returned_at->diffInDays($dueDate);
+    }
+
+    public function calculateFine(): float
+    {
+        $daysLate = $this->daysLate();
+        return $daysLate * self::finePerDay;
     }
 }
