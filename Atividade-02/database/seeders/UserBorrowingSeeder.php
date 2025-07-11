4fa86595616bd6fb1e47f2d5424377227610465b
Isaac <isaacluiz0808@gmail.com>
2025-07-11 09:54:38 -0300

    Adjusting UserBorrowingSeeder to allow the borrow until 5 limit. ensuring newly seeded borrowings are marked as 'in open' ('returned_at' is null) and by attempting to borrow non-borrowed books

--- Atividade-02/database/seeders/UserBorrowingSeeder.php
+++ Atividade-02/database/seeders/UserBorrowingSeeder.php
@@ -13,10 +13,23 @@ class UserBorrowingSeeder extends Seeder
     public function run()
     {
         User::factory(10)->create()->each(function ($user) {
-            Borrowing::factory(rand(1, 5))->create([
-                'user_id' => $user->id,
-                'book_id' => Book::inRandomOrder()->first()->id,
-            ]);
+            $numberOfBorrowings = rand(1, 5);
+            for ($i = 0; $i < $numberOfBorrowings; $i++) {
+                $book = Book::whereDoesntHave('users', function ($query) {
+                                $query->whereNull('returned_at');
+                            })
+                            ->inRandomOrder()
+                            ->first();
+                if ($book) {
+                    Borrowing::factory()->create([
+                        'user_id' => $user->id,
+                        'book_id' => $book->id,
+                        'returned_at' => null,
+                    ]);
+                } else {
+                    break;
+                }
+            }
         });
     }
 }
